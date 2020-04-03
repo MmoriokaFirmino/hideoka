2a81009b6f4c747e3242abd93ceecf0bc1fef8ab
MmoriokaFirmino <62939575+MmoriokaFirmino@users.noreply.github.com>
2020-04-03 17:19:17 +0900

    login.php

---
+++ login.php
@@ -0,0 +1,172 @@
+<?php
+
+//共通変数・関数ファイルを読込み
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「　ログインページ　');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+//ログイン認証
+require('auth.php');
+
+//================================
+// ログイン画面処理
+//================================
+// post送信されていた場合
+if(!empty($_POST)){
+  debug('POST送信があります。');
+
+  //変数にユーザー情報を代入
+  $email = $_POST['email'];
+  $pass = $_POST['pass'];
+  $pass_save = (!empty($_POST['pass_save'])) ? true : false; //ショートハンド（略記法）という書き方
+
+  //emailの形式チェック
+  validEmail($email, 'email');
+  //emailの最大文字数チェック
+  validMaxLen($email, 'email');
+
+  //パスワードの半角英数字チェック
+  validHalf($pass, 'pass');
+  //パスワードの最大文字数チェック
+  validMaxLen($pass, 'pass');
+  //パスワードの最小文字数チェック
+  validMinLen($pass, 'pass');
+  
+  //未入力チェック
+  validRequired($email, 'email');
+  validRequired($pass, 'pass');
+
+  if(empty($err_msg)){
+    debug('バリデーションOKです。');
+    
+    //例外処理
+    try {
+      // DBへ接続
+      $dbh = dbConnect();
+      // SQL文作成
+      $sql = 'SELECT password,id  FROM users WHERE email = :email';
+      $data = array(':email' => $email);
+      // クエリ実行
+      $stmt = queryPost($dbh, $sql, $data);
+      // クエリ結果の値を取得
+      $result = $stmt->fetch(PDO::FETCH_ASSOC);
+      
+      debug('クエリ結果の中身：'.print_r($result,true));
+      
+      // パスワード照合
+      if(!empty($result) && password_verify($pass, array_shift($result))){
+        debug('パスワードがマッチしました。');
+        
+        //ログイン有効期限（デフォルトを１時間とする）
+        $sesLimit = 60*60;
+        // 最終ログイン日時を現在日時に
+        $_SESSION['login_date'] = time(); //time関数は1970年1月1日 00:00:00 を0として、1秒経過するごとに1ずつ増加させた値が入る
+        
+        // ログイン保持にチェックがある場合
+        if($pass_save){
+          debug('ログイン保持にチェックがあります。');
+          // ログイン有効期限を30日にしてセット
+          $_SESSION['login_limit'] = $sesLimit * 24 * 30;
+        }else{
+          debug('ログイン保持にチェックはありません。');
+          // 次回からログイン保持しないので、ログイン有効期限を1時間後にセット
+          $_SESSION['login_limit'] = $sesLimit;
+        }
+        // ユーザーIDを格納
+        $_SESSION['user_id'] = $result['id'];
+        
+        debug('セッション変数の中身：'.print_r($_SESSION,true));
+        debug('マイページへ遷移します。');
+        header("Location:mypage.html"); //マイページへ
+      }else{
+        debug('パスワードがアンマッチです。');
+        $err_msg['common'] = MSG09;
+      }
+
+    } catch (Exception $e) {
+      error_log('エラー発生:' . $e->getMessage());
+      $err_msg['common'] = MSG07;
+    }
+  }
+}
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+?>
+    
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>ホームページのタイトル</title>
+    <style>
+        body{
+            margin: 0 auto;
+            padding: 150px;
+            width: 25%;
+            background: #fbfbfa;
+        }
+        h1{ color: #545454; font-size: 20px;}
+        form{
+            overflow: hidden;
+        }
+        input[type="text"]{
+            color: #545454;
+            height: 60px;
+            width: 100%;
+            padding: 5px 10px;
+            font-size: 16px;
+            display: block;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+      input[type="password"]{
+            color: #545454;
+            height: 60px;
+            width: 100%;
+            padding: 5px 10px;
+            font-size: 16px;
+            display: block;
+            margin-bottom: 10px;
+            box-sizing: border-box;
+        }
+        input[type="submit"]{
+            border: none;
+            padding: 15px 30px;
+            margin-bottom: 15px;
+            background: #3d3938;
+            color: white;
+            float: right;
+        }
+        input[type="submit"]:hover{
+            background: #111;
+            cursor: pointer;
+        }
+        a{
+            color: #545454;
+            display: block;
+        }
+        a:hover{
+            text-decoration: none;
+        }
+      .err_msg{
+        color: #ff4d4b;
+      }
+    </style>
+  </head>
+  <body>
+ 
+        <h1>ログイン</h1>
+            <form method="post" action="mypage.php">
+ 
+                <input type="text" name="email" placeholder="email" value="<?php if(!empty($_POST['email'])) echo $_POST['email'];?>">
+ 
+                <input type="password" name="pass" placeholder="パスワード" value="<?php if(!empty($_POST['pass'])) echo $_POST['pass'];?>">
+ 
+                <input type="submit" value="送信">
+ 
+            </form>
+ 
+  </body>
+</html>
